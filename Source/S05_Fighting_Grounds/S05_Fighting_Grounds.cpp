3def2b63c717e2962d145c91740a62039c7d2417
Carddeath <harry.brockley@hotmail.co.uk>
2017-05-03 15:52:29 +0100

    Using LFS for Uasset and Umap in unreal

---
+++ Source/S05_Fighting_Grounds/S05_Fighting_Grounds.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "S05_Fighting_Grounds.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, S05_Fighting_Grounds, "S05_Fighting_Grounds" );
+ 
---
+++ Source/S05_Fighting_Grounds/S05_Fighting_GroundsGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "S05_Fighting_Grounds.h"
+#include "S05_Fighting_GroundsGameMode.h"
+#include "S05_Fighting_GroundsHUD.h"
+#include "S05_Fighting_GroundsCharacter.h"
+
+AS05_Fighting_GroundsGameMode::AS05_Fighting_GroundsGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AS05_Fighting_GroundsHUD::StaticClass();
+}
